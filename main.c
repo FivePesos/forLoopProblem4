2be601be65341a97fb9525622ec5e6c3ae43b440
SingCow <75522316+FivePesos@users.noreply.github.com>
2023-12-12 21:31:19 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    for(int i = 1; i <= 5; i++){
+        for(int x = 1; x <= i; x++){
+            printf("%d " , x);
+        }
+        printf("\n");
+    }
+    return 0;
+}
